195822d9db577864b22d48b93cd308b671fe1351
Devi Amaolo <deviamaolo@gmail.com>
2022-03-23 20:12:24 -0300

    creating crytoapi with redux

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,38 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+    'X-RapidAPI-Key': '45c7a8d98cmsh3d7f93d3c98bd7bp11f461jsn8ee9d745de0d'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/coins';
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+})
+
+// var options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/coins',
+//     params: {
+//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       timePeriod: '24h',
+//       tiers: '1',
+//       orderBy: 'marketCap',
+//       orderDirection: 'desc',
+//       limit: '50',
+//       offset: '0'
+//     },
+//     headers: {
+//       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+//       'X-RapidAPI-Key': '45c7a8d98cmsh3d7f93d3c98bd7bp11f461jsn8ee9d745de0d'
+//     }
+//   };
