7e228154f9e135e8fd079ee601698d7015f39004
Devi Amaolo <deviamaolo@gmail.com>
2022-03-26 01:25:37 -0300

    creating some heading in homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,6 +3,7 @@ import millify from 'millify';
 import { Typography, Row, Col, Statistic} from 'antd';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import { Cryptocurrencies, News } from '../components';
 const {Title} = Typography
 
 const Homepage = () => {
@@ -26,6 +27,16 @@ const Homepage = () => {
             <Col span={12}><Statistic title='Total 24h Volume' value={millify(globalStats.total24hVolume)} /></Col>
             <Col span={12}><Statistic title='Total Markets' value={millify(globalStats.totalMarkets)} /></Col>
         </Row>
+        <div className="home-heading-container">
+          <Title level={2} className="home-title">Top 10 cryptocurrencies in the world</Title>
+          <Title level={3} className="show-more">Show more</Title>
+        </div>
+        <Cryptocurrencies simplified/>
+        <div className="home-heading-container">
+          <Title level={2} className="home-title">Latest Crypto news</Title>
+          <Title level={3} className="show-more">Show more</Title>
+        </div>
+        <News simplified/>
     </>
   )
 }
