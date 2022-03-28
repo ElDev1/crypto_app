414e2c880586954356423e1678a11d0561b53bab
Devi Amaolo <deviamaolo@gmail.com>
2022-03-28 00:11:12 -0300

    fixing error fetching data

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react'
 import millify from 'millify';
 import { Link } from 'react-router-dom';
 import {Card, Row, Col, Input} from 'antd';
-import { useGetCryptosQuery }  from '../services/cryptoApi';
+import { useGetCryptosQuery } from '../services/cryptoApi';
+
 
 const Cryptocurrencies = () => {
-  const { data: cryptosList, isFetching} = useGetCryptosQuery();
+  const { data: cryptosList, isFetching} = useGetCryptosQuery(10);
   const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
 
+  console.log('data from coins')
   console.log(cryptos);
 
   return (
