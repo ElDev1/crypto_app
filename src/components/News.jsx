3a7ada9a0e9eb1c4b06f9b66dfff629e71e6c508
Devi Amaolo <deviamaolo@gmail.com>
2022-03-29 00:48:21 -0300

    adding cryptonewsapi to reducer

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, {usesState} from 'react'
+import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
+import moment from 'moment';
 
-const News = () => {
+import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
+
+const { text, title } = Typography;
+const { option } = Select;
+
+const News = ({simplified}) => {
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: "cryptocurrency", count: simplified ? 6 : 12 });
+
   return (
     <div>News</div>
   )
